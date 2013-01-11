5de0cce0e9f94baa8723290e1858546a846025c1
hkjing <hkjing@flyaudio.cn>
2013-01-11 12:05:14 +0800

    增加tc358746驱动电流，有意设置tw9912输出30hz帧率

--- mywork/msm8x25/videoin/tc358746xbg_config.h
+++ mywork/msm8x25/videoin/tc358746xbg_config.h
@@ -36,6 +36,11 @@ static struct TC358_register_struct PALp_init_tab[]=
 
 {	0x0022	,	1440 	,	register_value_width_16		},   //	5A0
 
+{0x0100,0x203,register_value_width_32},//clck current 
+{0x0104,0x203,register_value_width_32},//lane0 current 
+{0x0108,0x203,register_value_width_32},//lane1 current 
+
+
 {	0x0140	,	0 	,	register_value_width_32		},   //	00000000
 
 {	0x0144	,	0 	,	register_value_width_32		},   //	00000000
@@ -87,6 +92,9 @@ static struct TC358_register_struct NTSCp_init_tab[]=
 {	0x0006	,	196 	,	register_value_width_16		},   //	00C4
 {	0x0008	,	96 	,	register_value_width_16		},   //	0060
 {	0x0022	,	1500 	,	register_value_width_16		},   //	5A0
+{0x0100,0x203,register_value_width_32},//clck current 
+{0x0104,0x203,register_value_width_32},//lane0 current 
+{0x0108,0x203,register_value_width_32},//lane1 current 
 {	0x0140	,	0 	,	register_value_width_32		},   //	00000000
 {	0x0144	,	0 	,	register_value_width_32		},   //	00000000
 {	0x0148	,	0 	,	register_value_width_32		},   //	00000000
