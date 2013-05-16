c9101162f930460f08e46f762922a81cf5751dc0
lisuwei <lisuweizhai@126.com>
2013-05-16 19:11:44 +0800

    io bug

--- drv/lidbg_io.c
+++ drv/lidbg_io.c
@@ -130,6 +130,15 @@ void mod_io_main(int argc, char **argv)
         u32 drive_strength;
         bool status;
 
+		if(argc < 3)
+		{
+			lidbg("Usage:\n");
+			lidbg("w index status\n");
+			lidbg("r index \n");
+			lidbg("i\n");
+			return;
+		}
+
         index = simple_strtoul(argv[1], 0, 0);
         //pull = simple_strtoul(argv[2], 0, 0);
         //drive_strength = simple_strtoul(argv[3], 0, 0);
