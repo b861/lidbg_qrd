7b97beed8354272a02e56403688386512992b5fb
lisuwei <lisuweizhai@126.com>
2013-05-29 17:55:51 +0800

    copy gt80x_update.ko to out_release

--- shell/copy_out_release.sh
+++ shell/copy_out_release.sh
@@ -25,3 +25,4 @@ cp $DBG_OUT_PATH/lidbg_gps_driver.ko $release_path
 cp $DBG_OUT_PATH/lidbg_ts_probe.ko $release_path
 cp $DBG_OUT_PATH/gt801.ko $release_path
 cp $DBG_OUT_PATH/gt811.ko $release_path
+cp $DBG_OUT_PATH/gt80x_update.ko $release_path
