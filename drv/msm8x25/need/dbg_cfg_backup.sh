5ca0b108e4fb34e5c9285cf4f718fcb5cd856598
lisuwei <lisuweizhai@126.com>
2013-06-14 18:02:59 +0800

    修改Makefile，简化编译方法,编译不用另外生成内核

--- drv/msm8x25/need/dbg_cfg_backup.sh
+++ drv/msm8x25/need/dbg_cfg_backup.sh
@@ -4,7 +4,7 @@ DBG_PLATFORM=flycar
 
 DBG_ROOT_PATH=/home/work/flyaudio/lidbg_qrd
 DBG_SYSTEM_DIR=/home/work/flyaudio/qrd_450/R8625SSNSKQLYA10045450
-DBG_KERNEL_DIR=/home/work/flyaudio/qrd_450/lidbg_kernel
+DBG_KERNEL_DIR=$DBG_SYSTEM_DIR/out/target/product/msm7627a/obj/KERNEL_OBJ
 
 DBG_CROSS_COMPILE=$DBG_SYSTEM_DIR/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 
