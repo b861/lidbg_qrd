718132437d2869f63b0578bab992ca4b699443c9
lisuwei <lisuweizhai@126.com>
2013-06-26 12:26:05 +0800

    clean.sh脚本完善

--- clean.sh
+++ clean.sh
@@ -4,5 +4,8 @@
 #       Date:         2012/02/03
 #=======================================================================================
 
-cd shell
-./all_clean.sh
+cd shell && ./all_clean.sh
+cd ..
+rm ./build_cfg.mk
+find . -name "*.ko" -type f -exec rm  {} \;
+
