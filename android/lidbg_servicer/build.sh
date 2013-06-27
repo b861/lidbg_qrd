21fad8a6ee8c90997a5443d067677687644f4bf4
lisuwei <lisuweizhai@126.com>
2013-06-27 16:04:07 +0800

    优化lidbg_servicer编译脚本

--- android/lidbg_servicer/build.sh
+++ android/lidbg_servicer/build.sh
@@ -1,4 +1,4 @@
+LOCATE_PATH=`pwd`
 source ../../dbg_cfg.sh
 cd ../../shell && source env_entry.sh
-cd $DBG_SYSTEM_DIR/&&source build/envsetup.sh&&choosecombo release $DBG_PLATFORM eng&&mmm $DBG_ROOT_PATH/android/lidbg_servicer
-
+cd $DBG_SYSTEM_DIR/&&source build/envsetup.sh&&choosecombo release $DBG_PLATFORM eng&&mmm $LOCATE_PATH
--- shell/build.sh
+++ shell/build.sh
@@ -53,7 +53,6 @@ source ./env_entry.sh
 	esac
 
 mkdir $DBG_OUT_PATH
-mkdir $DBG_ANDROID_OUT_PATH
 mkdir -p $DBG_OUT_RELEASE_PATH/$1/out
 
 #cd shell
