9255770adafc38b4f07c4a9efad42f13d2e94c22
lisuwei <lisuweizhai@126.com>
2013-06-29 12:01:44 +0800

    修改编译脚本名字

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 # for example
 #./make.sh mywork/msm8x25/videoin/
-cd shell && source env_entry.sh && ./build.sh $BOARD_VERSION $BUILD_VERSION
+cd shell && source env_entry.sh && ./build_cfg.sh $BOARD_VERSION $BUILD_VERSION
 echo start build $DBG_ROOT_PATH/$1
 cd $DBG_ROOT_PATH/$1 && make modules
 mv $DBG_ROOT_PATH/$1/*.ko $DBG_OUT_PATH/
