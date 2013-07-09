a45159815f369767ab0ae90cf12040049f61b905
lisuwei <lisuweizhai@126.com>
2013-07-09 10:07:04 +0800

    push.sh脚本修改ko权限，防止在android4.1启动的时候无法加载

--- build/push.sh
+++ build/push.sh
@@ -3,5 +3,6 @@ source ./env_entry.sh
 adb  wait-for-device remount
 adb push $DBG_OUT_PATH /system/lib/modules/out
 adb push $DBG_ANDROID_OUT_PATH/lidbg_servicer /system/bin/lidbg_servicer
+adb shell chmod 0644 /system/lib/modules/out/*.ko
 adb shell chmod 777 /system/bin/lidbg_servicer
 adb shell rm -r /flysystem/lib/out
