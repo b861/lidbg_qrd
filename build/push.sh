ad81f4dd66e3959e0db01659086c442b73982987
BNGao <576761368@qq.com>
2013-07-11 14:34:53 +0800

    push之前清空旧的.ko

--- build/push.sh
+++ build/push.sh
@@ -1,6 +1,7 @@
 
 source ./env_entry.sh
 adb  wait-for-device remount
+adb shell rm /system/lib/modules/out/*.ko
 adb push $DBG_OUT_PATH /system/lib/modules/out
 adb push $DBG_ANDROID_OUT_PATH/lidbg_servicer /system/bin/lidbg_servicer
 adb shell chmod 0644 /system/lib/modules/out/*.ko
