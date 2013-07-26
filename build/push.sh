20efe7f79d71e3722e1f795e221c779a713017f7
lisuwei <lisuweizhai@126.com>
2013-07-26 10:04:07 +0800

    fix 添加sync和reboot操作,防止数据丢失

--- build/push.sh
+++ build/push.sh
@@ -2,11 +2,11 @@
 source ./env_entry.sh
 adb  wait-for-device remount
 adb shell rm /system/lib/modules/out/*.ko
-sync
+adb shell sync
 adb push $DBG_OUT_PATH /system/lib/modules/out
 adb push $DBG_ANDROID_OUT_PATH/lidbg_servicer /system/bin/lidbg_servicer
 adb shell chmod 777 /system/lib/modules/out/*.ko
 adb shell chmod 777 /system/bin/lidbg_servicer
 adb shell rm -r /flysystem/lib/out
-sync
+adb shell sync
 adb reboot
--- build/pushfly.sh
+++ build/pushfly.sh
@@ -2,11 +2,11 @@
 source ./env_entry.sh
 adb  wait-for-device remount
 adb shell rm /flysystem/lib/out/*.ko
-sync
+adb shell sync
 adb push $DBG_OUT_RELEASE_PATH/$BOARD_VERSION/out /flysystem/lib/out
 adb push $DBG_ANDROID_OUT_PATH/lidbg_servicer /flysystem/bin/lidbg_servicer
 adb shell chmod 777 /flysystem/lib/out/*.ko
 adb shell chmod 777 /flysystem/bin/lidbg_servicer
 adb shell rm -r /system/lib/modules/out
-sync
+adb shell sync
 adb reboot
