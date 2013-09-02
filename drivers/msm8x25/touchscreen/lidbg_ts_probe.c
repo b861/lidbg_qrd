191dca25c7fcac2e75aa7e7f9246ca9b78571d33
lisuwei <lisuweizhai@126.com>
2013-09-02 10:19:13 +0800

    gt911兼容

--- drivers/msm8x25/touchscreen/lidbg_ts_probe.c
+++ drivers/msm8x25/touchscreen/lidbg_ts_probe.c
@@ -21,9 +21,9 @@ struct probe_device
 
 struct probe_device ts_probe_dev[] =
 {
-    {0x5d, 0x00, LOG_CAP_TS_GT811,"gt811.ko"}, //flycar
-    {0x55, 0x00, LOG_CAP_TS_GT801,"gt801.ko"}, //flycar
-//{0x14, 0x00, LOG_CAP_TS_GT911,"gt911.ko"}, //flycar
+    {0x5d, 0x00, LOG_CAP_TS_GT811,"gt811.ko"},
+    {0x55, 0x00, LOG_CAP_TS_GT801,"gt801.ko"},
+    {0x14, 0x00, LOG_CAP_TS_GT911,"gt911.ko"},
 //{0x5d, 0x00, LOG_CAP_TS_GT910,"gt910new.ko"}, //flycar  
 };
 
