47fd0fb88f20cfe2283723aa1bae28de48f1a635
lisuwei <lisuweizhai@126.com>
2013-09-28 21:53:49 +0800

    添加需要拷贝的release文件

--- build/all_make.sh
+++ build/all_make.sh
@@ -5,7 +5,7 @@
 #=======================================================================================
 source ./env_entry.sh
 hostname > $DBG_OUT_PATH/build_time.txt
-date >> $DBG_OUT_PATH/build_time.txt
+date >> $DBG_DRV_PATH/build_time.txt
 ./drv_make_copy.sh  &&./work_make_copy_all.sh
 
 
