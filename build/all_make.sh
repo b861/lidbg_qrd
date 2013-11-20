c6819c3441bd96c7b21dcc67b5e209ba23e0616d
lisuwei <lisuweizhai@126.com>
2013-11-20 16:51:16 +0800

    添加编译信息记录

--- build/all_make.sh
+++ build/all_make.sh
@@ -6,6 +6,9 @@
 source ./env_entry.sh
 hostname > $DBG_DRV_PATH/build_time.conf
 date >> $DBG_DRV_PATH/build_time.conf
+echo $DBG_PLATFORM >> $DBG_DRV_PATH/build_time.conf
+echo $BOARD_VERSION >> $DBG_DRV_PATH/build_time.conf
+echo $BUILD_VERSION >> $DBG_DRV_PATH/build_time.conf
 git log --oneline | sed -n '1p' >> $DBG_DRV_PATH/build_time.conf
 ./drv_make_copy.sh  &&./work_make_copy_all.sh
 
