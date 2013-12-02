2565530c6060220aed6c671b933f84520f0e426f
futengfei <2220929658@qq.com>
2013-12-02 14:30:49 +0800

    U盘拷贝走调试信息后清理logcat和ksmg文件

--- core/lidbg_misc.c
+++ core/lidbg_misc.c
@@ -146,6 +146,8 @@ void cp_data_to_udisk(char *key, char *value )
                 }
             }
         }
+        lidbg_rm("/data/logcat.txt");
+        lidbg_rm("/data/kmsg.txt");
     }
     else
         LIDBG_ERR("<nobody_register>\n");
