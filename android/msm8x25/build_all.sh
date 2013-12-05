a6a0664b1181e7d5a9b2478fd171f3bad413303b
lisuwei <lisuweizhai@126.com>
2013-12-05 16:23:57 +0800

    修复copylist.conf没有拷贝到release目录问题

--- android/msm8x25/build_all.sh
+++ android/msm8x25/build_all.sh
@@ -2,7 +2,8 @@ source ../../dbg_cfg.sh
 LOCATE_PATH=`pwd`
 cd ../../build && source env_entry.sh && ./build_cfg.sh $BOARD_VERSION $BUILD_VERSION $DBG_SOC
 cd $DBG_SYSTEM_DIR/&&source build/envsetup.sh&&choosecombo release $DBG_PLATFORM eng
-
+mkdir -p $DBG_ANDROID_OUT_PATH
+cp -u $DBG_ANDROID_PATH/$DBG_SOC/*.conf    $DBG_ANDROID_OUT_PATH/
 for each_dir in `ls -l $LOCATE_PATH | grep "^d" | awk '{print $NF}'`
 
 do
