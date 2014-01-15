20ae7c275dd3a95a6ebccddc1b33ce3b4748a561
lisuwei <lisuweizhai@126.com>
2014-01-15 11:29:09 +0800

    acc添加安全时钟

--- tools/print_key_log.sh
+++ tools/print_key_log.sh
@@ -55,7 +55,7 @@ do
 	echo ------------------------------------------------------------------------  >>  ./lidbg_log/lowmemkill.txt
 	cat $i | grep "send sigkill to" >> ./lidbg_log/lowmemkill.txt
 
-	cat $i | grep -E "block unsafe clk:33|lock unsafe clk:34|block unsafe clk:14|block wakelock|send sigkill to|killing any children in process group|acc_correct:send power_key|bp:my|Linux version|bp:reset|bp:pwr=|clear user wakelock|f8_timeout" >>  ./lidbg_log/$i
+	cat $i | grep -E "block unsafe clk:33|lock unsafe clk:34|block unsafe clk:14|block wakelock|send sigkill to|killing any children in process group|acc_correct:send power_key|bp:my|Linux version|bp:reset|bp:pwr=|clear user wakelock|f8_timeout|quick_resume" >>  ./lidbg_log/$i
 done
 
 
