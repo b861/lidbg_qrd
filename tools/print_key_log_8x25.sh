1764006edd6b8947ec38e6ac443b42724ad8a963
lisuwei <lisuweizhai@126.com>
2014-01-20 11:33:51 +0800

    分析调试信息脚本添加完成标志

--- tools/print_key_log_8x25.sh
+++ tools/print_key_log_8x25.sh
@@ -9,5 +9,5 @@ do
 	cat $i | grep -E "block unsafe clk:33|lock unsafe clk:34|block unsafe clk:14|block wakelock|send sigkill to|killing any children in process group|acc_correct:send power_key|bp:my|Linux version|bp:reset|bp:pwr=|clear user wakelock|f8_timeout|quick_resume|Freezing of tasks failed after|lidbgerr|LPC: RESET Wince" >>  ./lidbg_log/$i
 done
 
-
+date >>  ./lidbg_log/finish.txt
 
