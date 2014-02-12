784bdbe87939dccae488458d52b5feb07ae8f27b
lisuwei <lisuweizhai@126.com>
2014-02-12 11:45:11 +0800

    解决二进制仓库提交时git add无法跟踪某些文件的问题

--- build/release.sh
+++ build/release.sh
@@ -22,6 +22,7 @@ else
 	expect $DBG_TOOLS_PATH/pull
 	cp -ru $DBG_OUT_RELEASE_PATH/$BOARD_VERSION/out  $UPDATA_BIN_DIR
 	git add .
+	git add -f ./driver
 	git commit -am $1
 	expect $DBG_TOOLS_PATH/push
 	gitk &
