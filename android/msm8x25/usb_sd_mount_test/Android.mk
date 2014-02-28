a9bb5efb621e85a92c56b56aa5a823d1225a20dd
futengfei <2220929658@qq.com>
2014-02-28 10:34:43 +0800

    去除build_time.conf多次拷贝+删除sb_sd_mount_test目录

--- android/msm8x25/usb_sd_mount_test/Android.mk
+++
@@ -1,9 +0,0 @@
-
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES += libcutils libutils 
-LOCAL_LDLIBS += -llog
-LOCAL_SRC_FILES:= \/par    usb_sd_mount_test.c
-LOCAL_MODULE_TAGS:= optional
-LOCAL_MODULE := usb_sd_mount_test
-include $(BUILD_EXECUTABLE)
