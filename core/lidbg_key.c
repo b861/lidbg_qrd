d7911a6de94aff6b9b7f90a9819cfa56efdd086f
zhengtengjiao <284506761@qq.com>
2014-04-19 11:40:20 +0800

    修复BUG：确保25，Q平台不HOME键映射不变

--- core/lidbg_key.c
+++ core/lidbg_key.c
@@ -18,9 +18,11 @@ void lidbg_key_report(u32 key_value, u32 type)
 {
     lidbg("key - key_value:%d\n", key_value);
 
+#ifdef SOC_msm8x25
+#else
 if(key_value == KEY_HOME)
 	key_value = KEY_HOMEPAGE;
-
+#endif
     if(type == KEY_PRESSED)
         lidbg("key - press\n");
     else if(type == KEY_RELEASED)
