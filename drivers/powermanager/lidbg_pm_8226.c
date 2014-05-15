d56f983682ae406c59ddf2697091fea1cfa684f1
futengfei <2220929658@qq.com>
2014-05-15 09:38:10 +0800

    默认初始化GPIO_APP_STATUS管脚，防止被LPC复位

--- drivers/powermanager/lidbg_pm_8226.c
+++ drivers/powermanager/lidbg_pm_8226.c
@@ -460,13 +460,11 @@ static int __init lidbg_pm_init(void)
     LIDBG_GET;
     set_func_tbl();
 
-    SOC_IO_Output(0, GPIO_WP, 0);
-    PM_WARN("<set GPIO_WP[%d] 0>\n\n", GPIO_WP);
-    if(!g_var.is_fly)
-    {
-	    SOC_IO_Output(0, GPIO_APP_STATUS, 0);
-	    PM_WARN("<set GPIO_APP_STATUS [%d] 0>\n\n", GPIO_APP_STATUS);
-    }
+	SOC_IO_Output(0, GPIO_WP, 0);
+	PM_WARN("<set GPIO_WP[%d] 0>\n\n", GPIO_WP);
+	SOC_IO_Output(0, GPIO_APP_STATUS, 0);
+	PM_WARN("<set GPIO_APP_STATUS [%d] 0>\n\n", GPIO_APP_STATUS);
+
     CREATE_KTHREAD(thread_usb_disk_enable_delay, NULL);
     lidbg_uevent_shell("echo 8  > /proc/sys/kernel/printk");
     platform_device_register(&lidbg_pm);
