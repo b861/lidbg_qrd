59dbdf705a1d780a08cf057d47761c8a6c8696a3
lisuwei <lisuweizhai@126.com>
2014-06-09 12:13:31 +0800

    修复编译基础包时自动拷贝lk,bp问题

--- tools/soc_msm8x26.sh
+++ tools/soc_msm8x26.sh
@@ -2,8 +2,8 @@ function soc_make_otapackage()
 {
 	echo $FUNCNAME
 # cp lk,bp to /device/qcom/msm8226/radio
-	cp -u $RELEASE_REPOSITORY/lk/emmc_appsboot.mbn  device/qcom/msm8226/radio/
-	cp -u $RELEASE_REPOSITORY/radio/* 	       device/qcom/msm8226/radio/
+	cp -u $RELEASE_REPOSITORY/lk/emmc_appsboot.mbn  $DBG_SYSTEM_DIR/device/qcom/msm8226/radio/
+	cp -u $RELEASE_REPOSITORY/radio/* 	        $DBG_SYSTEM_DIR/device/qcom/msm8226/radio/
 	cd $DBG_SYSTEM_DIR
 	if [[ $TARGET_PRODUCT = "" ]];then
 		source build/envsetup.sh&&choosecombo release $DBG_PLATFORM $SYSTEM_BUILD_TYPE
