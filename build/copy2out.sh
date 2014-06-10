af0be0b4417e2276f2705fed20f1280f02300d99
lisuwei <lisuweizhai@126.com>
2014-06-10 18:00:00 +0800

    优化编译脚本

--- build/copy2out.sh
+++ build/copy2out.sh
@@ -26,8 +26,13 @@ for each_dir in `ls -l | grep "^d" | awk '{print $NF}'`
 	src_dir=$DBG_DRIVERS_PATH/$each_dir
 	cp -u $src_dir/*.ko     $dest_dir/	&> /dev/null
 	cp -u $src_dir/*.conf     $dest_dir/	&> /dev/null
-	cat $src_dir/dbg.confi  >> $dest_dir/drivers.conf   &> /dev/null
-	cat $src_dir/state.confi  >> $dest_dir/state.conf   &> /dev/null
+
+if [ -s $src_dir/dbg.confi ]; then
+	cat $src_dir/dbg.confi  >> $dest_dir/drivers.conf
+fi
+if [ -s $src_dir/state.confi ]; then
+	cat $src_dir/state.confi  >> $dest_dir/state.conf
+fi
 done
 
 cd $dest_dir
