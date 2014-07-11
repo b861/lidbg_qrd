d6282555123b9dd014c4d12e7156f7a39f7df652
lisuwei <lisuweizhai@126.com>
2014-07-11 16:46:57 +0800

    phone suspend test

---
+++ hal/lidbg_test/lidbg_test.c
@@ -0,0 +1,23 @@
+
+#include "lidbg_servicer.h"
+
+
+int main(int argc, char **argv)
+{
+	system("chmod 777 /sys/class/leds/button-backlight/brightness");
+	system("chmod 777 /sys/power/wake_lock");
+	system("chmod 777 /sys/power/wake_unlock");
+	while(1)
+	{
+	    system("echo lidbg > /sys/power/wake_lock");
+	    system("echo 1 > /sys/class/leds/button-backlight/brightness");
+	    sleep(1);
+
+
+	    system("echo 0 > /sys/class/leds/button-backlight/brightness");
+	    system("echo lidbg > /sys/power/wake_unlock");
+	    sleep(2);
+	}
+   	 return 0;
+}
+
