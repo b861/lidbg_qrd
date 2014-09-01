6478e38b7c9de20a378bd1e3576b9e02247764a4
zhengtengjiao <zhengtengjiao@flyaudio.cn>
2014-09-01 17:05:54 +0800

    兼容广百GT911

--- drivers/touchscreen/gt9xx.c
+++ drivers/touchscreen/gt9xx.c
@@ -941,6 +941,8 @@ u8 cfg_info_group15[] = CTP_CFG_GROUP9;
 u8 cfg_info_group16[] = CTP_CFG_GROUP10;
 u8 cfg_info_group17[] = CTP_CFG_GROUP11;
 u8 cfg_info_group18[] = CTP_CFG_GROUP12;
+u8 cfg_info_group19[] = CTP_CFG_GROUP13;
+u8 cfg_info_group20[] = CTP_CFG_GROUP14;
 static int gtp_init_panel(struct goodix_ts_data *ts, char *ic_type)
 {
 	struct i2c_client *client = ts->client;
@@ -955,7 +957,9 @@ static int gtp_init_panel(struct goodix_ts_data *ts, char *ic_type)
 	
 	u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
 		cfg_info_group3, cfg_info_group4,cfg_info_group5, cfg_info_group6,
-		cfg_info_group7, cfg_info_group8,cfg_info_group9, cfg_info_group10,cfg_info_group11, cfg_info_group12,cfg_info_group13, cfg_info_group14,cfg_info_group15, cfg_info_group16,cfg_info_group17, cfg_info_group18};
+		cfg_info_group7, cfg_info_group8,cfg_info_group9, cfg_info_group10,cfg_info_group11, 
+		cfg_info_group12,cfg_info_group13, cfg_info_group14,cfg_info_group15, cfg_info_group16,
+		cfg_info_group17, cfg_info_group18,cfg_info_group19, cfg_info_group20};
 
 	u8 cfg_info_len[] = {CFG_GROUP_LEN(cfg_info_group1),
 		CFG_GROUP_LEN(cfg_info_group2),
@@ -974,7 +978,9 @@ static int gtp_init_panel(struct goodix_ts_data *ts, char *ic_type)
 		CFG_GROUP_LEN(cfg_info_group15),
 		CFG_GROUP_LEN(cfg_info_group16),
 		CFG_GROUP_LEN(cfg_info_group17),
-		CFG_GROUP_LEN(cfg_info_group18)};
+		CFG_GROUP_LEN(cfg_info_group18),
+		CFG_GROUP_LEN(cfg_info_group19),
+		CFG_GROUP_LEN(cfg_info_group20)};
 
 #ifdef SOC_msm8x26
 	if(g_var.hw_info.hw_version == 1)
