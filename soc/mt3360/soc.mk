7edb9500ce98e7c5299972c3de3e698d1ec87dfa
BNGao <gaobenneng@flyaudio.cn>
2014-09-25 11:09:30 +0800

    验证pwm接口成功

--- soc/mt3360/soc.mk
+++ soc/mt3360/soc.mk
@@ -1,7 +1,8 @@
 EXTRA_CFLAGS += -DBUILD_SOC
 EXTRA_CFLAGS += -DNOT_USE_MEM_LOG
 
+EXTRA_CFLAGS += -I/media/D/flyaudio/ac8317/autochips/platform/kernel/drivers/inc
+EXTRA_CFLAGS += -I$(DBG_SYSTEM_DIR)/autochips/platform/kernel/drivers/inc
 EXTRA_CFLAGS += -I$(DBG_KERNEL_SRC_DIR)/arch/arm/mach-ac83xx/include
 EXTRA_CFLAGS += -I$(DBG_KERNEL_SRC_DIR)/arch/arm/mach-ac33xx/include/mach
 EXTRA_CFLAGS += -I$(DBG_KERNEL_SRC_DIR)/../autochips/kernel/arch/arm/mach-ac33xx/include/mach
-EXTRA_CFLAGS += -I$(DBG_KERNEL_SRC_DIR)/../autochips/platform/kernel/drivers/inc
