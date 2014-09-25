c20bc9132fd086651585c43f4b0ffe28728fb7f6
wangyihong <you@example.com>
2014-09-25 04:35:44 +0000

    recovery中获取触屏坐标驱动

--- drivers/touchscreen/lidbg_ts_to_recov.c
+++ drivers/touchscreen/lidbg_ts_to_recov.c
@@ -1,73 +1,25 @@
 
+#include "recoverytouch.h"
+#include "lidbg.h"
 
-/*
- * 12linux/drivers/serial/tcc_dev_cypress.c
- anxiao_cap_dev.ko
- deyi----gooddix.ko
- zhongchu------gh.ko
- qimei ------qnovox.ko
+//#include "../../globalDef.h"
 
+#define TOUCH_MODE_MMAP    11
+#define TOUCH_MODE_NORMAL  22
+#define TOUCH_MODE_SEL     TOUCH_MODE_NORMAL
 
- * Author: <linux@telechips.com>
- * Created: June 10, 2008
- * Description: Touchscreen driver for devC2003 on Telechips TCC Series
- *
- * Copyright (C) 2008-2009 Telechips
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see the file COPYING, or write
- * to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-#include <linux/kernel.h>
-#include <linux/device.h>
-#include <linux/hrtimer.h>
-#include <linux/delay.h>
-#include <linux/platform_device.h>
-#include <linux/init.h>
-#include <linux/input.h>
-#include <linux/interrupt.h>
-#include <linux/module.h>
-#include <linux/types.h>
-#include <linux/fs.h>
-#include <linux/errno.h>
-#include <linux/clk.h>
-#include <linux/irq.h>
-#include <linux/slab.h>
-#include <linux/miscdevice.h>
-#include <linux/spinlock.h>
-#include <linux/i2c.h>
-//#include <linux/tcc_dev_cypress.h>
-#ifdef CONFIG_HAS_EARLYSUSPEND
-#include <linux/earlysuspend.h>
-#endif
-
-//#include <asm/io.h>
-//#include <asm/irq.h>
-#include <asm/system.h>
-#include <asm/uaccess.h>
-//#include <mach/hardware.h>
-
-//#include <mach/bsp.h>
-//#include <mach/irqs.h>
-
-#include <linux/cdev.h>
-#include "touch.h"
-#ifdef SOC_msm8x25
-
-static touch_t touch = {0, 0, 0};
 
 #define DEVICE_NAME "fenzhi"
+volatile touch_t *touch;
+
+touch_t *touchtemp;
+
+#include <linux/spinlock.h>
+DEFINE_SPINLOCK(touchlock);
+
+
+bool bNeedRead;
+wait_queue_head_t read_wait;
 
 struct test_input_dev
 {
@@ -78,18 +30,25 @@ struct test_input_dev
     struct miscdevice *miscdev;
 };
 
+bool bPress = false;
 void set_touch_pos(touch_t *t)
 {
-#if 0
-    int i = 0;
-    i = touch.x;
-    touch.x = touch.y;
-    touch.y = i;
-#endif
-    memcpy( &touch, t, sizeof(touch_t) );
+    if (bPress == t->pressed)
+    {
+		return;
+    }
+
+	memcpy(touch, t, sizeof(touch_t));
+
+	bPress = touch->pressed;
+
+ 	spin_lock(&touchlock);
+	bNeedRead = true;
+	spin_unlock(&touchlock);
+	wake_up_interruptible(&read_wait);
 }
 
-EXPORT_SYMBOL_GPL(set_touch_pos);
+EXPORT_SYMBOL(set_touch_pos);
 
 //global variable
 struct test_input_dev *dev;
@@ -100,21 +59,56 @@ static void dev_timer_func(unsigned long arg)
     mod_timer(&dev->timer, jiffies + HZ);
     (dev->counter)++;
     if(dev->counter > 60)dev->counter = 0;
-    printk("touch.x:%d touch.y:%d touch.press:[%d]\n", touch.x, touch.y, touch.pressed);
+    //printk("touch.x:%d touch.y:%d touch.press:[%d]\n", touch.x, touch.y, touch.pressed);
     //printk("second%d\n", dev->counter);
     //input_report_abs(dev->input_dev, ABS_X, dev->counter);
     //input_report_abs(dev->input_dev, ABS_Y, dev->counter);
     //input_sync(dev->input_dev);
 }
 
+unsigned int fenzhi_poll(struct file *filp, poll_table *wait)
+{
+	unsigned int mask = 0;
+
+	if (!bNeedRead)
+	{
+		//printk("\n fenzhi_poll %d",bNeedRead);
+		poll_wait(filp, &read_wait, wait);	
+		//interruptible_sleep_on(&read_wait);
+	}
+
+	if (bNeedRead)
+	{
+#if TOUCH_MODE_SEL == TOUCH_MODE_MMAP
+		bNeedRead = false;
+#endif
+
+		mask |= POLLIN|POLLRDNORM;
+	}
+
+	return mask;
+}
 
 int fenzhi_open (struct inode *inode, struct file *filp)
 {
-    //do nothing
+    printk("lidbg_ts_to_recov.ko  Open. \n");
     return 0;          /* success */
 }
 
+static int fenzhi_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+	vma->vm_pgoff = ((unsigned long)virt_to_phys(touch)) >> PAGE_SHIFT;  
 
+	printk("vma->vm_pgoff = %lx\n",vma->vm_pgoff); 
+
+	if (remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff, sizeof(touch_t),  vma->vm_page_prot))  
+	{
+		printk("remap_pfn_range error\n");
+		return -EAGAIN; 
+	}
+
+	return 0;
+}
 
 /*
  * Data management: read and write
@@ -122,15 +116,20 @@ int fenzhi_open (struct inode *inode, struct file *filp)
 static status = 0;
 ssize_t fenzhi_read (struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
-    struct test_input_dev *dev = filp->private_data;
-
-    if(sizeof(touch_t) > count)return 0;
-
-    if( !(touch.x || touch.y) )return 0;
-
-    copy_to_user(buf, &touch, sizeof(touch_t));
-
-    return sizeof(touch_t);
+	//printk("\n fenzhi_read %d",bNeedRead);
+ 	spin_lock(&touchlock);
+	if (bNeedRead)
+	{
+		bNeedRead = false;
+		spin_unlock(&touchlock);
+		copy_to_user(buf, touch, sizeof(touch_t));
+		return sizeof(touch_t);
+	}
+	else
+	{
+		spin_unlock(&touchlock);
+		return 0;
+	}
 }
 
 
@@ -138,25 +137,31 @@ ssize_t fenzhi_read (struct file *filp, char __user *buf, size_t count, loff_t *
 ssize_t fenzhi_write (struct file *filp, const char __user *buf, size_t count,
                       loff_t *f_pos)
 {
-    struct test_input_dev *dev = filp->private_data;
 
     return 0;
 }
 
-struct file_operations fenzhi_fops =
+
+static  struct file_operations fenzhi_fops =
 {
     .owner =     THIS_MODULE,
-    .read =	     fenzhi_read,
+    .read  =	 fenzhi_read,
     .write =     fenzhi_write,
-    .open =	     fenzhi_open,
+	.poll  =	 fenzhi_poll,
+    .open  =	 fenzhi_open,
+	.mmap  =	 fenzhi_mmap,
+
 };
 
-static struct miscdevice misc =
+
+int thread_recov_init(void *data)
 {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = DEVICE_NAME,
-    .fops = &fenzhi_fops,
-};
+    lidbg_new_cdev(&fenzhi_fops, DEVICE_NAME);
+	
+    return 0;
+}
+
+
 
 //static int __devinit
 //static int initialized = 0;
@@ -164,52 +169,45 @@ static struct miscdevice misc =
 static  int my_input_driver_init(void)
 {
     int ret;
+    DUMP_BUILD_TIME;
+    //LIDBG_GET;
+\
+
+
 
     //printk("my_input_driver_init,write by hujian 2012/2/8 16:45");
-    printk(KERN_CRIT"lidbg_ts_to_recov.ko  my_input_driver_init. \n");
-    dev = (struct test_input_dev *)kmalloc( sizeof(struct test_input_dev), GFP_KERNEL );
-    if (dev == NULL)
-    {
-        ret = -ENOMEM;
-        printk(KERN_CRIT "%s: Failed to allocate input device\n", __func__);
-        return ret;
-    }
+    lidbg("xxxxxlidbg_ts_to_recov.ko  my_input_driver_init. \n");
 
-    //miscdev
-    dev->miscdev = &misc;
+	CREATE_KTHREAD(thread_recov_init, NULL);
 
-    ret = misc_register(&misc);
-    if(ret < 0 )
-    {
-        kfree(dev);
-        return ret;
-    }
 
-#if 0
-    init_timer(&dev->timer);
-    dev->timer.data = (unsigned long)dev;
-    dev->timer.function = dev_timer_func;
-    dev->timer.expires = jiffies + HZ;
+	bNeedRead = false;
+	init_waitqueue_head(&read_wait);
 
-    add_timer(&dev->timer);
-    printk("\n\nTest input driver has launch successfully!\n\n");
-#else
-    printk(KERN_CRIT"\n\nTest input driver has launch successfully!_delete the time ==futengfei=1025\n\n");
+#if  TOUCH_MODE_SEL == TOUCH_MODE_MMAP
+	touchtemp = (touch_t *)kmalloc(sizeof(touch_t), GFP_KERNEL);
+	touch = (touch_t *)(PAGE_ALIGN((unsigned long)touchtemp));
+	memset(touch,0,sizeof(touch_t));
+#elif TOUCH_MODE_SEL == TOUCH_MODE_NORMAL
+	touch = (touch_t *)kmalloc(sizeof(touch_t), GFP_KERNEL);
 #endif
-    return 0;
 
+	lidbg("lidbg_ts_to_recov.ko  my_input_driver_init. OK\n");
+
+    return 0;
 }
 
 //void __exit
 static void my_input_driver_exit(void)
 {
-    misc_deregister(&misc);
-    del_timer(&dev->timer);
-    //input_unregister_device(dev->input_dev);
-    kfree(dev);
+
 }
 //EXPORT_SYMBOL_GPL(my_input_driver_init);
 //EXPORT_SYMBOL_GPL(my_input_driver_exit);
+
+
+
+
 
 module_init(my_input_driver_init);
 module_exit(my_input_driver_exit);
@@ -217,25 +215,4 @@ module_exit(my_input_driver_exit);
 MODULE_AUTHOR("hujian");
 MODULE_DESCRIPTION("input driver,just for test");
 MODULE_LICENSE("GPL");
-#else
-#include "lidbg.h"
-LIDBG_DEFINE;
 
-void set_touch_pos(touch_t *t){}
-void set_touch_pos2(struct tspara *touch){}
-
-static  int my_input_driver_init(void)
-{
-    LIDBG_GET;
-    plidbg_dev->soc_func_tbl.pfnSOC_Set_Touch_Pos = set_touch_pos2;
-
-	return 0;
-}
-module_init(my_input_driver_init);
-
-EXPORT_SYMBOL_GPL(set_touch_pos);
-MODULE_AUTHOR("fly");
-MODULE_DESCRIPTION("input driver,just for test");
-MODULE_LICENSE("GPL");
-
-#endif
