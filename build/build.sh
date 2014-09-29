03c8b8e7798d6162f57fa5ecae8ceba109e93c60
lisuwei <lisuweizhai@126.com>
2014-09-29 12:05:00 +0800

    优化自动编译脚本

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,10 @@ source ./env_entry.sh
 ./build_cfg.sh $DBG_SOC $BOARD_VERSION $DBG_PLATFORM
 mkdir -p $DBG_OUT_PATH
 
+find ../ -name "Module.symvers" -exec rm -rf {} \;
+./make_all.sh  && . $DBG_BUILD_PATH/copy2out.sh 
+
+
 build_log=$DBG_OUT_PATH/build_time.conf
 hostname > $build_log
 date >> $build_log
@@ -10,8 +14,5 @@ echo $DBG_PLATFORM >> $build_log
 echo $BOARD_VERSION >> $build_log
 echo $BUILD_VERSION >> $build_log
 git log --oneline | sed -n '1,10p' >> $build_log
-
-find ../ -name "Module.symvers" -exec rm -rf {} \;
-./make_all.sh  && . $DBG_BUILD_PATH/copy2out.sh 
 
 
