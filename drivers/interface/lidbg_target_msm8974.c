3cc2ed431dd2bab7a887d2119c9850f3e0743001
zhengtengjiao <zhengtengjiao@flyaudio.cn>
2014-09-30 11:36:04 +0800

    匹配fly_parameter分区编号

--- drivers/interface/lidbg_target_msm8974.c
+++ drivers/interface/lidbg_target_msm8974.c
@@ -76,7 +76,7 @@ struct hw_version_specific g_hw_version_specific[] =
 	.cpu_freq_recovery_limit = "600000",
 	.sensor_num = 5,
 
-	.fly_parameter_node = "/dev/block/mmcblk0p25",
+	.fly_parameter_node = "/dev/block/mmcblk0p18",
 	},
 {//msm8974 v2
 	.gpio_lcd_reset = 80,
@@ -158,7 +158,7 @@ struct hw_version_specific g_hw_version_specific[] =
 	.sensor_num = 5,
 	.fan_onoff_temp = 75,
 
-	.fly_parameter_node = "/dev/block/mmcblk0p25",
+	.fly_parameter_node = "/dev/block/mmcblk0p18",
 	},
 
 };
