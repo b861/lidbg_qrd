340f001b0b9c92fe9488bbf4f5f96747809e4122
lisuwei <lisuweizhai@126.com>
2014-12-09 12:48:42 +0800

    mkfs.exfat使用bin文件

--- hal/8x26_vold/lidbg_vold/Android.mk
+++ hal/8x26_vold/lidbg_vold/Android.mk
@@ -1,5 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-
+include $(CLEAR_VARS)
+LOCAL_MODULE := mkfs.exfat
+LOCAL_SRC_FILES := bin/mkfs.exfat
+LOCAL_MODULE_CLASS := bin
+LOCAL_MODULE_TAGS := optional debug eng tests samples
+LOCAL_MODULE_PATH := $(DBG_OUT_PATH)
+include $(BUILD_PREBUILT)
 
 include $(call all-makefiles-under, $(LOCAL_PATH))
