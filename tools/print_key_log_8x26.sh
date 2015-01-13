c12142f7c96e71fe6287527034a0760dccfd10fd
lisuwei <lisuweizhai@126.com>
2015-01-13 16:09:29 +0800

    完善分析调试信息脚本

--- tools/print_key_log_8x26.sh
+++ tools/print_key_log_8x26.sh
@@ -23,7 +23,19 @@ do
 	cat $i | grep "7.suspend_devices_and_enter" >>  ./lidbg_log/acc_count.txt
 
 
-	cat $i | grep -E "send sigkill to|killing any children in process group|Linux version|force_unlock" >>  ./lidbg_log/$i
+	echo ---------------------------$i---------------------------------------------  >> ./lidbg_log/WARNING.txt
+	cat $i | grep "WARNING: at" >>  ./lidbg_log/WARNING.txt
+
+	echo ---------------------------$i---------------------------------------------  >> ./lidbg_log/unhandled.txt
+	cat $i | grep "unhandled page" >>  ./lidbg_log/unhandled.txt
+
+	echo ---------------------------$i---------------------------------------------  >> ./lidbg_log/lidbgerr.txt
+	cat $i | grep "lidbgerr" >>  ./lidbg_log/lidbgerr.txt
+
+	echo ---------------------------$i---------------------------------------------  >> ./lidbg_log/slimbus.txt
+	cat $i | grep "slimbus Read error" >>  ./lidbg_log/slimbuserr.txt
+
+	cat $i | grep -E "send sigkill to|killing any children in process group|Linux version|force_unlock|WARNING: at|slimbus Read error|unhandled page|lidbgerr" >>  ./lidbg_log/$i
 done
 
 	date >>  ./lidbg_log/finish.txt
