c7169a50269b1da875f29b91e3fcc0f9f0f71938
futengfei <2220929658@qq.com>
2015-02-03 15:17:26 +0800

    使能USB摄像头功能

--- hal/hal_camera_usb/Android.mk
+++ hal/hal_camera_usb/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-#include $(DBG_BUILD_PATH)/build_cfg.mk
-LOCAL_MODULE := camera.msm8226
-#LOCAL_MODULE_PATH := $(DBG_OUT_PATH)
+include $(DBG_BUILD_PATH)/build_cfg.mk
+LOCAL_MODULE := camera.$(DBG_PLATFORM)
+#LOCAL_MODULE := camera.default
 LOCAL_MODULE_PATH := $(DBG_OUT_PATH)
 LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc \
         hardware/qcom/media/libstagefrighthw
