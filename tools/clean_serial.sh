f4660791a45feca4f64b1fcd5fe91525c857c82d
lisuwei <lisuweizhai@126.com>
2015-04-18 13:54:12 +0800

    修复打开串口错误提示

---
+++ tools/clean_serial.sh
@@ -0,0 +1,15 @@
+ls /dev/ttyUSB* 
+date > /dev/shm/ttyUSB0.txt
+date > /dev/shm/ttyUSB1.txt
+date > /dev/shm/ttyUSB2.txt
+date > /dev/shm/ttyUSB3.txt
+date > /dev/shm/ttyUSB4.txt
+date > /dev/shm/ttyUSB5.txt
+date > /dev/shm/ttyUSB6.txt
+date > /dev/shm/ttyUSB7.txt
+date > /dev/shm/ttyUSB8.txt
+date > /dev/shm/ttyUSB9.txt
+date > /dev/shm/ttyUSB10.txt
+date > /dev/shm/ttyUSB11.txt
+date > /dev/shm/ttyUSB12.txt
+date > /dev/shm/ttyUSB13.txt
