ae309f111b7db8b0679ee7117f33ffcf2fb42f9b
futengfei <2220929658@qq.com>
2015-04-22 16:58:59 +0800

    补刀lk几率开背光无效的问题

---
+++ drivers/build_in/lidbg_i2c.c
@@ -0,0 +1,64 @@
+#include <linux/i2c.h>
+#include <linux/delay.h>
+#include <linux/kthread.h>
+
+
+int cmn_i2c_write(int i2c_bus_id, u8 dev_address_7bit, u8 *data, int len)
+{
+    int ret;
+    struct i2c_msg msgs[] =
+    {
+        {
+            .addr = dev_address_7bit,
+            .flags = 0,//write
+            .len = len,
+            .buf = data,
+        },
+    };
+    for(ret = 0; ret < len; ret++)
+        printk("=========data[%d][%x]\n", ret, *(data + ret));
+    ret = i2c_transfer(i2c_get_adapter(i2c_bus_id), msgs, 1);
+    if (ret < 0)
+        printk(KERN_CRIT"=========i2c_transfer.err\n");
+    return ret;
+}
+int lpc_send_cmd(u8 *writebuf, int writelen)
+{
+    int cmdlen = 3 + 1 + writelen;
+    int i;
+    u8 cmd[cmdlen];
+
+    cmd[cmdlen - 1] = 0;
+    printk("%d,%d\n", writelen, sizeof(cmd));
+    cmd[0] = 0xff;
+    cmd[1] = 0x55;
+    cmd[2] = writelen + 1;
+    memcpy(&cmd[3], writebuf, writelen);
+
+    for(i = 2; i < cmdlen - 1; i++)
+        cmd[cmdlen - 1] += cmd[i];
+    return cmn_i2c_write(0, 0xa0 >> 1, cmd, cmdlen);
+}
+
+
+void lcd_on(void)
+{
+    u8 buff[] = {0x02, 0x0d, 0x1};
+    printk(KERN_CRIT"===lcd_on===\n");
+    lpc_send_cmd(buff, sizeof(buff));
+}
+
+
+static int thread_wait_i2c(void *data)
+{
+    allow_signal(SIGKILL);
+    allow_signal(SIGSTOP);
+    ssleep(1);
+    lcd_on();
+    return 1;
+}
+void lidbg_i2c_start(void)
+{
+	kthread_run(thread_wait_i2c, NULL, "ftf_filepolltask");
+}
+
--- drivers/build_in/lidbg_kernel.c
+++ drivers/build_in/lidbg_kernel.c
@@ -5,13 +5,16 @@
 
 #include "cmn_func.c"
 #include "boot_freq_ctrl.c"
+#include "lidbg_i2c.c"
 
 LIDBG_THREAD_DEFINE;
 
 int __init lidbg_kernel_init(void)
 {
 	DUMP_BUILD_TIME;
+    printk(KERN_CRIT"===lidbg_kernel_init===\n");
 	freq_ctrl_start();
+	lidbg_i2c_start();
 	LIDBG_GET_THREAD;
 	return 0;
 }
