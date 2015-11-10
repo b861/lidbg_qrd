19c156fa5f4da947cdf1ce6fbd1fb13f648f24a1
ctb <CTB@flyaudio.cn>
2015-11-10 16:03:47 +0800

    编译lk前先删除out目录下的lk生成文件

--- tools/soc_msm8x26.sh
+++ tools/soc_msm8x26.sh
@@ -198,6 +198,7 @@ function soc_build_bootloader()
 
 	cp -rf $DBG_ROOT_PATH/fly_bootloader/* $DBG_BOOTLOADER_DIR/flyaudio
 	cp -f $DBG_ROOT_PATH/build/build_cfg.mk $DBG_BOOTLOADER_DIR/flyaudio/common/build_cfg.mk
+	rm -rf $DBG_SYSTEM_DIR/out/target/product/$DBG_PLATFORM/obj/EMMC_BOOTLOADER_OBJ/
 	echo DEFINES += $(echo BOOTLOADER_$DBG_PLATFORM | tr '[a-z]' '[A-Z]') >> $DBG_BOOTLOADER_DIR/flyaudio/common/build_cfg.mk
 
 	set_env
