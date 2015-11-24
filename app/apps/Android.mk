a45b3b5977983bbcc68494610aea6be378921f7e
BNGao <BNGao>
2015-11-24 15:10:58 +0800

    还原FlyBootService，添加新结构开关机app代码

--- app/apps/Android.mk
+++ app/apps/Android.mk
@@ -1,5 +1,6 @@
 
 #SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,FastPowerOn)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,LidbgPmService)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,FlyBootService)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,SleepTest)
 include $(SUBDIR_MAKEFILES)
