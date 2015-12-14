d91e90b0a7c0295d1304698869b2279ab3a2f7e0
lisuwei <lisuweizhai@126.com>
2015-12-14 15:31:38 +0800

    修改8909测试包服务器路径

--- build/env_entry.sh
+++ build/env_entry.sh
@@ -372,7 +372,7 @@ case "$DBG_PLATFORM_ID" in
 	export DBG_VENDOR=VENDOR_QCOM
 	export OTA_PACKAGE_NAME=msm8909-ota-eng.*.zip
 	export DBG_SOC=msm8x26
-	export TEST_PACKAGE_PATH=//192.168.128.128/8909/升级包发布/专项测试包
+	export TEST_PACKAGE_PATH=//192.168.128.128/share/8909/升级包/测试升级包
 	export REPOSITORY_WORK_BRANCH=master
 	export SYSTEM_WORK_BRANCH=msm8909-master
         export MAKE_PAKG_NUM=18
