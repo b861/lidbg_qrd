ac796df0d2631580c16b34b83b49625a1ade84b5
BNGao <BNGao>
2015-12-31 16:06:56 +0800

    删除多余的app代码

--- app/apps/Android.mk
+++ app/apps/Android.mk
@@ -2,8 +2,6 @@
 #SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,FastPowerOn)
 ifeq ($(CONFIG_APP_LIDBGPMSERVICE),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,LidbgPmService)
-else ifeq($(CONFIG_APP_SUSPEND_UNAIRPLANEMODE), y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,SuspendUnairplanemode)
 else
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,FlyBootService)
 endif
