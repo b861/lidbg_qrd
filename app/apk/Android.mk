5e169fe647b9d19260fa0c595d9e6ddb9987e142
futengfei <2220929658@qq.com>
2016-01-05 14:26:32 +0800

    增加自动打电话和接收到来电或者短信就开启录像的APK

--- app/apk/Android.mk
+++ app/apk/Android.mk
@@ -43,6 +43,14 @@ LOCAL_MODULE_PATH := $(DBG_OUT_PATH)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := CallMessage.ko
+LOCAL_SRC_FILES := CallMessage.apk
+LOCAL_MODULE_CLASS := bin
+LOCAL_MODULE_TAGS := optional debug eng tests samples
+LOCAL_MODULE_PATH := $(DBG_OUT_PATH)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := ftpd
 LOCAL_SRC_FILES := ftpd
 LOCAL_MODULE_CLASS := bin
