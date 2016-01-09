20a9740c83654039ab452c317b534f8a7691d9aa
futengfei <540720599@qq.com>
2016-01-09 11:53:29 +0800

    添加百度直播APK+收到短信和电话响铃后打开5V延迟10S开启百度直播APK50S后关5V杀百度释放锁

--- drivers/drivers_dbg/lidbg_drivers_cmd.c
+++ drivers/drivers_dbg/lidbg_drivers_cmd.c
@@ -313,6 +313,7 @@ void parse_cmd(char *pt)
             lidbg_pm_install(get_lidbg_file_path(buff, "ST.ko"));
             lidbg_pm_install(get_lidbg_file_path(buff, "GPS.ko"));
             lidbg_pm_install(get_lidbg_file_path(buff, "camera4hal.ko"));
+            lidbg_pm_install(get_lidbg_file_path(buff, "LiveSessionDemo.ko"));
             lidbg_pm_install(get_lidbg_file_path(buff, "CallMessage.ko"));
             lidbg_domineering_ack();
         }
