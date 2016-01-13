2fc086f523d6654bdbea9ffb9b8c86bbc0b22494
BNGao <gaobenneng@flyaudio.cn>
2016-01-13 14:46:31 +0800

    打印系统异常唤醒统计

--- drivers/powermanager/lidbg_rmtctrl.c
+++ drivers/powermanager/lidbg_rmtctrl.c
@@ -266,6 +266,8 @@ static int unormal_wakeup_handle(void)
 
 		system_unormal_wakeup_cnt++;
 
+		system_tics = get_tick_count() - system_unormal_wakeuped_tics;  //tics ms after acc_off
+		lidbg("*** ystem wakeup %u(%u) times in %d(%u) msec ***\n", system_unormal_wakeup_cnt, UNORMAL_WAKEUP_CNT, system_tics, (UNORMAL_WAKEUP_TIME_MINU * 60 * 1000));
 		if(system_unormal_wakeup_cnt > UNORMAL_WAKEUP_CNT){
 			repeat_times++;
 			if(repeat_times >= 5)
@@ -274,7 +276,6 @@ static int unormal_wakeup_handle(void)
 				ssleep(10);
 				lidbg_shell_cmd("reboot");
 			}
-			system_tics = get_tick_count() - system_unormal_wakeuped_tics;  //tics ms after acc_off
 			if(system_tics < (UNORMAL_WAKEUP_TIME_MINU * 60 * 1000)){
 				lidbgerr("System wakeup %d times in %d(%u) msec,system tics %u, unormal\n", system_unormal_wakeup_cnt, system_tics, (UNORMAL_WAKEUP_TIME_MINU * 60 * 1000), get_tick_count());
 				if(acc_io_state == FLY_ACC_OFF)
