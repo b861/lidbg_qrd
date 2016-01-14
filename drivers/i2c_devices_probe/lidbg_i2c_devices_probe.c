2550ada9c21290f8733df85869e859678cf40743
futengfei <540720599@qq.com>
2016-01-14 14:49:59 +0800

    修正冷启动读ACC状态异常的问题

--- drivers/i2c_devices_probe/lidbg_i2c_devices_probe.c
+++ drivers/i2c_devices_probe/lidbg_i2c_devices_probe.c
@@ -156,6 +156,7 @@ int i2c_devices_probe_thread(void *data)
 {
     int cnt = 3;
 #ifdef SUSPEND_ONLINE	
+    g_var.acc_flag =  SOC_IO_Input(MCU_ACC_STATE_IO, MCU_ACC_STATE_IO, GPIO_CFG_PULL_UP);
     LIDBG_WARN("<g_var.acc_flag1:%d>\n",g_var.acc_flag);
     while(g_var.acc_flag==0)
     {
