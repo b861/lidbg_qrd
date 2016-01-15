d7ed61c2a9391c949caafbbda47b4d1b9cd52b11
futengfei <540720599@qq.com>
2016-01-15 20:30:13 +0800

    增加获取系统bootcomplete广播的方式

--- app/lidbg_android_server/android_server.cpp
+++ app/lidbg_android_server/android_server.cpp
@@ -16,7 +16,7 @@
 #include <math.h>
 #include <system/audio.h>
 
-#define TAG "sound:"
+#define TAG "android_server:"
 using namespace android;
 
 bool dbg = false;
@@ -30,34 +30,52 @@ void GetAudioPolicyService(bool dbg)
     sp<IServiceManager> sm = defaultServiceManager();
     sp<IBinder> binder;
     if(dbg)
-        lidbg( TAG"GetAudioPolicyService.in");
+        lidbg( TAG"GetAudioPolicyService.in\n");
 
     do
     {
         binder = sm->getService(String16("media.audio_policy"));
         if (binder != 0)
             break;
-        lidbg(TAG" waiting...");
+        lidbg(TAG" waiting...\n");
         usleep(1000000);
     }
     while (true);
     gAudioPolicyService = interface_cast<IAudioPolicyService>(binder);
     if(gAudioPolicyService == 0)
-        lidbg(TAG "GetAudioPolicyService.fail1");
+        lidbg(TAG "GetAudioPolicyService.fail1\n");
     else if(dbg)
-        lidbg( TAG"GetAudioPolicyService.succes1");
+        lidbg( TAG"GetAudioPolicyService.succes1\n");
 }
 
 bool playing = false;
+static void *thread_check_boot_complete(void *data)
+{
+	while(1)
+	{
+	        char value[PROPERTY_VALUE_MAX];
+	        property_get("sys.boot_completed", value, "0");
+	        if (value[0] == '1')
+	        {
+	        	lidbg( TAG" send message  :boot_completed = %c,delay 2S \n",value[0]);
+		sleep(2);
+	        	LIDBG_WRITE("/dev/lidbg_interface", "BOOT_COMPLETED");
+		break;
+	        }
+		sleep(1);
+	}
+    return ((void *) 0);
+}
 int main(int argc, char **argv)
 {
+    pthread_t ntid;
     argc = argc;
     argv = argv;
-    lidbg( TAG"lidbg_android_server:main");
+    lidbg( TAG"lidbg_android_server:main\n");
 
     sleep(20);
     GetAudioPolicyService(true);
-
+    pthread_create(&ntid,NULL,thread_check_boot_complete,NULL);
     while(1)
     {
         if(gAudioPolicyService != 0)
@@ -73,7 +91,7 @@ int main(int argc, char **argv)
         }
         else
         {
-            lidbg( TAG"gAudioPolicyService == 0");
+            lidbg( TAG"gAudioPolicyService == 0\n");
             GetAudioPolicyService(true);
         }
 
@@ -99,7 +117,8 @@ int main(int argc, char **argv)
                 dbg = 0;
             loop_count = 0;
         }
-        usleep(100000);
+
+        usleep(100000);//100ms
     }
 
 }
