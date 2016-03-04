39503a3bff3aeea08e5a36d0414759f690eca8f6
futengfei <540720599@qq.com>
2016-03-04 16:28:49 +0800

    添加通知链效率时间统计

--- core/lidbg_notifier.c
+++ core/lidbg_notifier.c
@@ -17,8 +17,12 @@ int unregister_lidbg_notifier(struct notifier_block *nb)
 int lidbg_notifier_call_chain(unsigned long val)
 {
     int ret;
-    lidbg("lidbg_notifier_call_chain:val=%ld,major=%ld,minor=%ld\n", val, val >> 16, val & 0xffff);
+    ktime_t start, diff;
+    start =ktime_get();
     ret = blocking_notifier_call_chain(&lidbg_chain_head, val, NULL);
+    diff = ktime_sub(ktime_get(), start);
+
+    lidbg("lidbg_notifier_call_chain:used time[%d]ms,val=%ld,major=%ld,minor=%ld\n", (int)ktime_to_ms(diff),val, val >> 16, val & 0xffff);
 
     return notifier_to_errno(ret);
 }
